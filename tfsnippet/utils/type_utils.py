b8431dffffbe78f8412ed6df01a286ccfe70c931
Yan_Bingxin_ <62475249+Yan9564@users.noreply.github.com>
2024-04-25 16:36:27 +0800

    Update type_utils.py

--- tfsnippet/utils/type_utils.py
+++ tfsnippet/utils/type_utils.py
@@ -18,7 +18,7 @@ __all__ = [
 
 __INTEGER_TYPES = (
     six.integer_types +
-    (np.integer, np.int, np.uint,
+    (np.integer, int, np.uint,
      np.int8, np.int16, np.int32, np.int64,
      np.uint8, np.uint16, np.uint32, np.uint64)
 )
